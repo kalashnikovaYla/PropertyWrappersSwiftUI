f454f924e1489a822cc2ab8b36d2527846c3c8ca
kalashnikovaYla <kalashnikova-yla@mail.ru>
2024-09-14 16:57:07 +0500

    intial

---
+++ PropertyWrappersSwiftUI/PropertyWrappersSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PropertyWrappersSwiftUIApp.swift
+//  PropertyWrappersSwiftUI
+//
+//  Created by Юлия  on 14.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct PropertyWrappersSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
